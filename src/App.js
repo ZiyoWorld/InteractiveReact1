065959dab18b65672da5e614a63c662f7ab059c2
Mirziyod_ms <mirziyodsunatillayev1612@gmail.com>
2022-04-04 00:43:39 +0300

    change img

--- src/App.js
+++ src/App.js
@@ -15,9 +15,13 @@ class App extends React.Component{
              <Footer /> */}
              <Header />
              <div className="main">
-             <Drinks />
-             <Drinks />
-             <Drinks />
+             <Drinks img="https://www.pngplay.com/wp-content/uploads/12/Strawberry-Juice-Transparent-File.png" name="Orange Juice" price="4.99" off="30" />
+             <Drinks img="https://freepikpsd.com/file/2019/10/juice-box-png-2-Transparent-Images.png" name="Apple Box" price="2.99" off="20" />
+             <Drinks img="https://pulpy.ru/api/uploads/c84de145fabd4562ae57eee0f8b40462.png" name="Pulpy red" price="1.59" off="-" />
+             <Drinks img="https://pulpy.ru/api/uploads/2d830813a08e4466bca42418946c1872.png" name="Pulpy yellow" price="1.59" off="-" />
+             <Drinks img="https://www.pngplay.com/wp-content/uploads/12/Strawberry-Juice-Transparent-File.png" name="Orange Juice" price="4.99" off="30" />
+             <Drinks img="https://www.pngplay.com/wp-content/uploads/12/Strawberry-Juice-Transparent-File.png" name="Orange Juice" price="4.99" off="30" />
+             
 
              </div>
              
--- src/Drinks.js
+++ src/Drinks.js
@@ -6,13 +6,13 @@ class Drinks extends React.Component{
         return(
             <div>
                 <div className="card">
-                        <img className="main-img" src="https://www.pngplay.com/wp-content/uploads/12/Strawberry-Juice-Transparent-File.png" alt="" />
-                         <h1>Orange Juice</h1>
+                        <img className="main-img" src={this.props.img} alt="" />
+                         <h1>{this.props.name}</h1>
                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                         <h1 className="price">$ 4.99</h1>
+                         <h1 className="price">$ {this.props.price}</h1>
                          <button type="button">VIEW PRODUCT</button>
                 <div className="products">
-                    <p>-30%</p>
+                    <p>-{this.props.off}%</p>
                 </div>
                 
                 </div>
